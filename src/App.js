6a599d4120bd69ebe999928b7420ce7cda93f69b
a-blaine <ai.blaine4@protonmail.com>
2023-05-02 18:14:22 +0100

    Removed unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <Weather />
+      <div class="footer">
+        This project was created by Ashley Blaine and is{" "}
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/a-blaine/react-weather"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noreferrer noopener"
         >
-          Learn React
-        </a>
-      </header>
+          open sourced
+        </a>{" "}
+        on GitHub
+      </div>
     </div>
   );
 }
