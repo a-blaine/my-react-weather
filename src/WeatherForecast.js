409a108568c81ee53734018bffc6628090a3d4d7
a-blaine <ai.blaine4@protonmail.com>
2023-05-09 09:07:39 +0100

    Updated forecast API key and URL

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -24,10 +24,10 @@ export default function WeatherForecast({ coordinates }) {
       </div>
     );
   } else {
-    const apiKey = "7ae5e58d29dbe83f5367ad389e4a99a2";
+    const apiKey = "3dce9b1c66837262a25b3f448d354a76";
     let longitude = coordinates.lon;
     let latitude = coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
 
     return null;
